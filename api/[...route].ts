c9db89276cffa36cb6380030084363202ae5f79d
juanpaulvdmerwe <juan-paul@hrsinsurance.co.za>
2026-06-15 10:35:41 +0200

    fix errors

--- api/[...route].ts
+++ api/[...route].ts
@@ -1,6 +1,6 @@
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
-import { db } from '../src/db'
+import { db } from '../src/db/index.ts'
 import { sql } from 'drizzle-orm'
 
 export const config = { runtime: 'edge' }
